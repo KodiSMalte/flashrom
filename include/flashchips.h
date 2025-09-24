63a9732abe8dca4f1aaf20606064302fbcbf55f0
KodiSMalte <malte.mannott@polizei.landsh.de>
2025-09-24 09:59:43 +0200

    header add PYUA chip

--- include/flashchips.h
+++ include/flashchips.h
@@ -658,11 +658,12 @@
 #define PMC_PM49FL002		0x6D
 #define PMC_PM49FL004		0x6E
 
-#define PUYA_ID			0x85
+#define PUYA_ID				0x85
 #define PUYA_P25Q06H		0x4010
 #define PUYA_P25Q11H		0x4011
 #define PUYA_P25Q21H		0x4012
 #define PUYA_P25D80H		0x6014
+#define PUYA_P25Q16SH 		0x6015
 
 /*
  * The Sanyo chip found so far uses SPI, first byte is manufacturer code,
